90d083ec0dccd9f1b0537d1c7557514887427e17
duyquang2301 <duyquang.tdq7@gmail.com>
2023-01-04 11:40:25 +0700

    responsive + js lanđingPage

--- Day_7_BTL/main.js
+++ Day_7_BTL/main.js
@@ -3,6 +3,7 @@ const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
 //functions 
+
 function handleClickEvents() {
     const contentBlocks = $$('.slide__content-item')
     contentBlocks.forEach((contentBlock, index) => {
@@ -15,11 +16,53 @@ function handleClickEvents() {
             slideImage.classList.add('img-active')
         }
     })
+
+    const cardHeaders = $$('.card-header')
+    const cardBodys = $$('.card-body')
+    //Click event in slide section
+    cardHeaders.forEach((cardHeader, index) => {
+        const cardBody = cardBodys[index]
+        cardHeader.onclick = function() {
+            $('.card-body.card-body-active').classList.remove('card-body-active')
+            
+            cardBody.classList.toggle('card-body-active')
+        }
+    })
 }
 
+function modalHandle() {
+    const main = $('.main')
+    const workCardImgs = $$('.work__card-img')
+    const workCards = $$('.card-wrap')
+    const modal = $('.modal')
+    const modalImg = $('.modal-content')
+    const modalContents = $$('.modal-content')
+    const closeBtn = $('.close-btn')
+
+    workCards.forEach((workCard, index) => {
+        const workCardImg = workCardImgs[index]
+        workCard.onclick = (e) => {
+            modal.style.display = 'block'
+            modalImg.src = workCardImg.src
+            e.stopPropagation()
+        }
+    })
+
+    main.onclick = () => {
+        modal.style.display = 'none'
+    }
+
+    modalContents.forEach((modalContent) => {
+        modalContent.onclick = (e) => {
+            e.stopPropagation()
+        }
+    })
+
+    closeBtn.onclick = () => {
+        modal.style.display = 'none'
+    }
+}
 handleClickEvents()
-
-
-
+modalHandle()
 
 
